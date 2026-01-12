04a123b96377c05da52bf8221ead037b278bacf1
Rutuja <rutujaghaytad1515@gmail.com>
2026-01-12 11:26:30 +0530

    Add Sale mongoose model

---
+++ backend/models/Sale.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const saleSchema = new mongoose.Schema(
+  {
+    product: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    revenue: {
+      type: Number,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Sale", saleSchema);
